d445ea751a758a9c513d6e9bcec4c8552ab3630f
Radhika403 <radhikabansal403@gmail.com>
2023-01-26 14:12:20 +0530

    api token

---
+++ bot.py
@@ -0,0 +1,58 @@
+import telegram.ext 
+
+def start(update, context):
+    update.message.reply_text("""Hello!
+    Type /help to see the available commands 
+    """)
+    
+def help(update,context):
+    update.message.reply_text("""
+    The following commands are avilable:
+    
+    /help -> This message
+    /hackathonDetails  -> Get all the details about the Hackathon!!
+    /registration -> Register yourself for the hackathon here
+    /resources -> Dive in to explore the resources
+    /queries -> Post your queries here
+    /reminders -> Set a reminder here
+    /contact -> contact information 
+    """)
+    
+def resources(update, context):
+    update.message.reply_text("TESTTT We have various playlists and articles available!")
+
+def hackathonDetails(update, context):
+    update.message.reply_text("Details of the hackathon are as follows")
+
+def registration(update, context):
+    update.message.reply_text("Follow the given steps t register yourself for the hackathon")
+    
+def queries(update, context):
+    update.message.reply_text("Please type your queries")
+    
+def reminders(update, context):
+    update.message.reply_text("Welcome to reminders")
+    
+def contact(update, context):
+    update.message.reply_text("You can contact on the official mail id")
+
+# def handle_message(update, context):
+#     update.message.reply_text(f"You said {update.message.text}, use the commands using /")
+
+
+Token = ("5650433111:AAGg_7eRAGaQJg7s4GKJg28PwdOmIYim7qU")
+#print(bot.get_me())
+updater = telegram.ext.Updater(Token, use_context=True)
+disp = updater.dispatcher
+
+disp.add_handler(telegram.ext.CommandHandler('start',start))
+disp.add_handler(telegram.ext.CommandHandler('help',help))
+disp.add_handler(telegram.ext.CommandHandler('hackathonDetails',hackathonDetails))
+disp.add_handler(telegram.ext.CommandHandler('registration',registration))
+disp.add_handler(telegram.ext.CommandHandler('resources',resources))
+disp.add_handler(telegram.ext.CommandHandler('queries',queries))
+disp.add_handler(telegram.ext.CommandHandler('reminders',reminders))
+disp.add_handler(telegram.ext.CommandHandler('contact',contact))
+# disp.add_handler(telegram.ext.MessageHandler(telegram.ext.Filters.text, handle_message))
+updater.start_polling()
+updater.idle()
